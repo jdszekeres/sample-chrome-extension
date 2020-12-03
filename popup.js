1bdd4e0aa040cac64be3a66c3b89e36de79978e2
jdszekeres <69050063+jdszekeres@users.noreply.github.com>
2020-12-02 16:16:02 -0800

    more nonsense

--- popup.js
+++ popup.js
@@ -1,5 +1,10 @@
 'use strict';
 function myfunction(arguments){
   dosomething();
+  something = dosomethingelse(arguments);
+  console.log(something/2);
   return something
 }
+//edit this
+//it will return an error
+
